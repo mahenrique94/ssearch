1185263c6808b5af8bc7b19214fc0992b2f4931d
Matheus <matheushcastiglioni@gmail.com>
2017-01-20 16:55:12 -0200

    Implementando recurso para select funcionar via teclado também

--- ssearch.js
+++ ssearch.js
@@ -38,15 +38,31 @@ class SSearch {
 		ssData.addEventListener('click', function() {
 			this.parentNode.style.height = '300px';
 			filter(ssData, list);
-			showList(list);
+			showElement(list);
 		});
 		ssData.addEventListener('focus', function() {
 			this.parentNode.style.height = '300px';
 			filter(ssData, list);
-			showList(list);
+			showElement(list);
 		});
-		ssData.addEventListener('keyup', function() {
-			filter(this, list);
+		ssData.addEventListener('keyup', function(event) {
+			if (event.keyCode === 38 || event.keyCode === 40) {
+				// seta para cima
+				if (event.keyCode === 38)
+					previusItem(list);
+				// seta para baixo
+				if (event.keyCode === 40)
+					nextItem(list);
+			} else {
+				filter(this, list);
+			}
+		});
+		ssData.addEventListener('keypress', function(event) {
+			// enter
+			if (event.keyCode === 13) {
+				event.preventDefault();
+				chooseItem(list);
+			}
 		});
 		this.data = ssData;
 		return ssData;
@@ -82,18 +98,23 @@ class SSearch {
 		item.textContent = text;
 		item.setAttribute('data-ss-value', value);
 		item.addEventListener('click', function() {
-			let evt = new Event('change');
-			let value = item.getAttribute('data-ss-value');
-			let filter = item.parentNode.parentNode.querySelector('input.ss-search__filter');
-			let select = item.parentNode.parentNode.parentNode.querySelector('select.ss-search');
-			filter.value = item.textContent;
-			select.value = value;
-			select.dispatchEvent(evt);
-			hideList(this.parentNode);
+			selectedItem(this);
 		});
 		return item;
 	}
 	
+}
+
+// Selecionando o item e definindo ele como valor do select
+function selectedItem(item) {
+	let evt = new Event('change');
+	let value = item.getAttribute('data-ss-value');
+	let filter = item.parentNode.parentNode.querySelector('input.ss-search__filter');
+	let select = item.parentNode.parentNode.parentNode.querySelector('select.ss-search');
+	filter.value = item.textContent;
+	select.value = value;
+	select.dispatchEvent(evt);
+	hideElement(item.parentNode);
 }
 
 // Filtrar os itens de acordo com o valor digitado no input
@@ -101,38 +122,84 @@ function filter(filter, list) {
 	let regExp = new RegExp(filter.value, 'gi');
 	let regExpMark = new RegExp('(([<])([\/]?[a-z]+([a-z0-9\s\=\"\-\_]*))([>]))', 'gim');
 	if (filter.value != '') {
+		removeHovered(list);
 		list.children.forEach(item => {
 			item.texContent = item.textContent.replace(regExpMark, '');
 			let matches = item.textContent.match(regExp);
 			if (matches != null && matches.length > 0) {
+				showElement(item);
 				item.style.display = 'block';
 				matches.forEach(match => item.innerHTML = item.textContent.replace(match, `<mark class="ss-search__match">${match}</mark>`));
 			} else {
 				item.style.display = 'none'; 
+				hideElement(item);
 			}
 		});
 	} else {
 		list.children.forEach(item => {
 			item.innerHTML = item.textContent.replace(regExpMark, '');
+			showElement(item);
 			item.style.display = 'block';
 		});
 	}
 }
 
-// Abrir a lista de itens para serem filtrados ou selecionados
-function showList(list) {
-	list.classList.remove('is-hide');
-	list.classList.add('is-show');
-	list.setAttribute('aria-hidden', 'false');
-	list.setAttribute('aria-expanded', 'true');
+function showElement(element) {
+	element.classList.remove('is-hide');
+	element.classList.add('is-show');
+	element.setAttribute('aria-hidden', 'false');
+	element.setAttribute('aria-expanded', 'true');
 }
 
-// Fechar a lista de itens
-function hideList(list) {
-	list.classList.remove('is-show');
-	list.classList.add('is-hide');
-	list.setAttribute('aria-hidden', 'true');
-	list.setAttribute('aria-expanded', 'false');
+function hideElement(element) {
+	element.classList.remove('is-show');
+	element.classList.add('is-hide');
+	element.setAttribute('aria-hidden', 'true');
+	element.setAttribute('aria-expanded', 'false');
+}
+
+// Ir para o item anterior da lista com as setas do teclado
+function previusItem(list) {
+	if (hasItemHovered(list)) {
+		const hover = findHovered(list);
+		hover.classList.remove('is-hovered');
+		hover.previousSibling.classList.add('is-hovered');
+	}
+}
+
+// Ir para o próximo item da lista com as setas do teclado
+function nextItem(list) {
+	if (hasItemHovered(list)) {
+		const hover = findHovered(list);
+		hover.classList.remove('is-hovered');
+		hover.nextSibling.classList.add('is-hovered');
+	} else {
+		const item = list.querySelector('.ss-search__item.is-show');
+		if (item.textContent != '') 
+			item.classList.add('is-hovered');
+		else
+			item.nextSibling.classList.add('is-hovered');
+	}
+}
+
+// Selecionar item na lsita com o enter
+function chooseItem(list) {
+	const hover = findHovered(list);
+	selectedItem(hover);
+}
+
+function findHovered(list) {
+	return list.querySelector('.is-hovered');
+}
+
+function hasItemHovered(list) {
+	return findHovered(list) != undefined;
+}
+
+function removeHovered(list) {
+	let hover = findHovered(list);
+	if (hover != undefined)
+		hover.classList.remove('is-hovered');
 }
 
 const selects = document.querySelectorAll('select.ss-search');
